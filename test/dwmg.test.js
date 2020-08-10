bf787b80dac16da03959e14a535b641fcc5fa877
p0isonra1n <isaacparker@gmail.com>
2020-08-10 11:34:21 +1000

    Add test for adding icon with and without type

--- test/dwmg.test.js
+++ test/dwmg.test.js
@@ -9,6 +9,9 @@ var manifest = new DWMG({
     description: "description"
 });
 
+manifest.addIcon("/image.png", "48x48");
+manifest.addIcon("/image2.png", "72x72", "image/png");
+
 test('All Variables set correctly', () =>{
     const exported = manifest.exportToJSON();
     expect(exported.name).toEqual("name");
@@ -17,4 +20,11 @@ test('All Variables set correctly', () =>{
     expect(exported.display).toEqual(DWMG.displayTypes.standalone);
     expect(exported.background_color).toEqual("#000000");
     expect(exported.description).toEqual("description");
+    expect(exported.icons.length).toBeGreaterThan(1);
+});
+
+test("Icon handling of blank type", () => {
+    const exported = manifest.exportToJSON();
+    expect(exported.icons[0].type).toBeUndefined();
+    expect(exported.icons[1].type).toBeDefined();
 });
